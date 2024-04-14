ec9cffa7a2a6f7a644c01bf4c537529afa5c2a0b
Anaherciu <ana.herciu@ulbsibiu.ro>
2024-04-14 18:57:50 +0300

    Added project test for implementing unit testing

---
+++ BibliotecaTest/ClassLibrary/BaseClass/DocumentTests.cs
@@ -0,0 +1,25 @@
+﻿using Biblioteca;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaTest.ClassLibrary.BaseClass
+{
+    internal class DocumentTests
+    {
+        [Test]
+        public void DocumentConstructor_WithTitle_SetsTitle()
+        {
+            string expectedTitle = "Test Document";
+
+
+            Document document = new Document(expectedTitle);
+
+
+            Assert.That(document.Title, Is.EqualTo(expectedTitle));
+        }
+    }
+}
